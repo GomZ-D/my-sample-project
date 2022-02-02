207592d51adb0d7278692605c32d6296ecad167a
GomZ-10 <gomezdiego09@gmail.com>
2022-02-01 22:18:14 -0500

    final project

--- tictactoe.py
+++ tictactoe.py
@@ -3,35 +3,49 @@ TicTacToe
 Author: Diego A. Gomez
 CSE 210
 '''
-
+import os
 
 def main ():
 
-    player1, player2 = players()
-    player = "x"
-   
+    # player1, player2 = players()
+    player = ""
+    count = 0
+    
     game = tictactoe()
    
-    while (winner(game)!=False):
-
-        print_tictactoe (game)  
-        num = player_move(player)
+   
+    while not (winner(game)==True or count == 9): 
+        os.system('cls')
         player = player_turn(player)
+        print_tictactoe (game)
+        num = player_move(player)
         game [num -1] = player
+        count = count + 1
+        outcome = winner(game)
+        champ = player
+    
+    
     print_tictactoe(game)
-    print("Good game. Thanks for playing!") 
+    tie(count,outcome,champ)    
+    print("Good game. Thanks for playing!")
 
+# def players ():
+#     player1 = input('Enter player 1 name: ')
+#     player2 = input('Enter player 2 name: ')
+#     return player1, player2
 
-def players ():
-    player1 = input('Enter player 1 name: ')
-    player2 = input('Enter player 2 name: ')
-    return player1, player2
-
+# def players (player):
+#     if player.upper() == "X":
+#         print('Player X is the Champion! Game Over')
+#     elif player.upper() == "O":
+#         print('Player O is the Champion! Game over')
+        
 def tictactoe():
     game=[]
     for num in range (9):
         game.append(num + 1)
     return game
+
 
 
 def print_tictactoe(game):
@@ -50,15 +64,25 @@ def print_tictactoe(game):
     print("\t {} | {} | {}".format(game[6], game[7], game[8]))
     print("\n")
 
+
 def player_move (player):
-    num = int(input(f"{player}'s turn to choose a square (1-9): "))
-    return num
+    validation = False
+    while validation == False:
+        num = input(f"{player}'s turn to choose a square (1-9): ")
+        if num == "1" or num == "2" or num == '3' or num == '4' or num == '5' or num == '6' or num == '7' or num == '8' or num== '9':
+            validation = True
+        else:
+            validation = False
+    num2 = int(num)
+    return num2
+
 
 def player_turn (turn):
-    if turn == "x":
-        return "o"
-    elif turn == "o":
-        return "x"
+    if turn == "" or turn.upper() == "O":
+        return "X"
+    elif turn.upper() == "X":
+        return "O"
+
 
 def winner (game):
     if (game[0] == game[1] == game[2] or game[3] == game[4] == game[5] or
@@ -69,8 +93,17 @@ def winner (game):
     else:
         return False
 
-# def tie (game):
+def tie (count,outcome,champ):
+    if count >= 9 and outcome == False:
+        print("It's a Tie!!")
+    elif outcome == True:
+        print(f"{champ} is the champion! ")
 
+    
+# def scoreboard (points, player):
+#     print("\t--------------------------------")
+#     print("\t              SCOREBOARD       ")
+#     print("\t--------------------------------")
 
 
 
